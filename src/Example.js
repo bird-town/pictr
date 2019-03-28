fc91c09710f5ef1610313a6987b184bc2b086a49
Jackson Callaway <jackson.callaway@coinbase.com>
2019-03-27 20:27:44 -0700

    create an Example component to present usage examples for Pictr

---
+++ src/Example.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Pictr from './Pictr';
+
+export default class Example extends Component {
+    render = () => {
+        return(
+            <Pictr />
+        )
+    }
+}
