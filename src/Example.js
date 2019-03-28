294897766ca41a396dc27b4344b16b3dea33f884
Jackson Callaway <jackson.callaway@coinbase.com>
2019-03-27 20:34:48 -0700

    add styled-components so we can easily embed css in js

--- src/Example.js
+++ src/Example.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
+import styled from 'styled-components'
 import Pictr from './Pictr';
+
+const ExamplesContainer = styled.div`
+width: 100vw;
+height: 100vh;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+`
 
 export default class Example extends Component {
     render = () => {
         return(
-            <Pictr />
+            <ExamplesContainer>
+                <Pictr />
+                <Pictr />
+                <Pictr />
+            </ExamplesContainer>
         )
     }
 }
